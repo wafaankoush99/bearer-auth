b9f14bdce2f232eb36f0f9d3ca0ebe65c296a663
wafaankoush99 <wafadirawe@gmail.com>
2021-05-28 01:08:52 +0300

    mod

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -17,7 +17,7 @@ authRouter.post('/signup', async (req, res, next) => {
       user: userRecord,
       token: userRecord.token,
     };
-    res.status(201).json(output);//200
+    res.status(201).json(output);
   } catch (e) {
     next(e.message);
   }
